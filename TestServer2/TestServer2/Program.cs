533f23d7e74dbbc78d7889fa04ccb07cd8af3832
marco <marcovannoord@gmail.com>
2016-02-18 13:29:29 +0100

    First commit

---
+++ TestServer2/TestServer2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BankContext())
+            {
+                Console.WriteLine("Voer de naam van de eerste klant in:");
+                string name = Console.ReadLine();
+                var Client = new Client { Name = name };
+                db.Clients.Add(Client);
+                db.SaveChanges();
+
+                Console.WriteLine("\nDe klanten zijn:");
+                foreach (var item in db.Clients)
+                {
+                    Console.WriteLine(item.Name);
+                }
+                Console.WriteLine();
+                Console.WriteLine("please enter the name of your new bank account");
+                var account = Console.ReadLine();
+                var Account = new Account { Client = Client, Title = account};
+                db.Accounts.Add(Account);
+                db.SaveChanges();
+                Console.WriteLine("\nDe rekeningen zijn:");
+                foreach (var item in db.Accounts)
+                {
+                    Console.WriteLine("rekeningnaam: " + item.Title);
+                    Console.WriteLine("eigenaar: " + item.Client.Name);
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Voer de naam in van de klant die je wilt zoeken");
+                var zoekstring = Console.ReadLine();
+                var GevondenKlant = db.Clients.Where(x => x.Name.Contains(zoekstring)).First();
+                Console.Write(GevondenKlant.Name);
+                Console.ReadKey();
+            }
+                
+        }
+    }
+    public class BankContext : DbContext
+    {
+        public BankContext() : base("MyDbContextConnectionString") { }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+    }
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Beschrijving { get; set; }
+
+        public virtual List<Account> Accounts { get; set; }
+    }
+
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int ClientId { get; set; }
+        public virtual Client Client { get; set; }
+    }
+    
+}
