641abf8bffeb8d1b06f72dbd3f18ca647bea0c85
marco <marcovannoord@gmail.com>
2016-03-14 13:58:09 +0100

    Meer comments

--- TestServer2/TestServer2/Program.cs
+++ TestServer2/TestServer2/Program.cs
@@ -28,6 +28,9 @@ namespace TestServer2
                 db.SaveChanges();
 
                 Console.WriteLine("\nDe klanten zijn:");
+
+                //we lopen door alle klanten heen. Op de achtergrond wordt er sql uitgevoerd.
+                //db.clients is een property, net als de properties die je onderaan hebt aangemaakt
                 foreach (var item in db.Clients)
                 {
                     Console.WriteLine(item.Name);
@@ -55,12 +58,20 @@ namespace TestServer2
                 
         }
     }
+
+    /// <summary>
+    /// door het aanmaken van een dbcontext zorg je dat er tabellen worden gemaakt met onderstaande namen Clients en Accounts
+    /// </summary>
     public class BankContext : DbContext
     {
         public BankContext() : base("MyDbContextConnectionString") { }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Account> Accounts { get; set; }
     }
+
+    /// <summary>
+    /// Dit is ons model van de klant. 
+    /// </summary>
     public class Client
     {
         public int ClientId { get; set; }
@@ -70,13 +81,22 @@ namespace TestServer2
         public virtual List<Account> Accounts { get; set; }
     }
 
+    /// <summary>
+    /// dit is het model van ons bankaccount
+    /// </summary>
     public class Account
     {
         public int AccountId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
 
+        /// <summary>
+        /// verwijzing naar de primary key van de klant
+        /// </summary>
         public int ClientId { get; set; }
+        /// <summary>
+        /// zo weet Entity Framework dat we een datatype Client gebruiken
+        /// </summary>
         public virtual Client Client { get; set; }
     }
     
